d8c4e93988d62092f6149655c7c26f68beb0fc11
Dilip <rexsunar37010@gmail.com>
2023-01-21 20:59:40 +0530

    Committed form-validation

--- lib/Pages/login_screen.dart
+++ lib/Pages/login_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:login_app/utils/routes.dart';
 
 class Login extends StatefulWidget {
@@ -14,6 +12,24 @@ class _LoginState extends State<Login> {
   String name = '';
   bool changeButton = false;
 
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      // await and future used as the change of screen to homepage
+      // has to be done after 1s only i.e after the transition of the animatedContainer
+      await Future.delayed(Duration(seconds: 1));
+      // await used in Navigator as after 1s so that the button again changes back to the text 'Login'
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,97 +38,96 @@ class _LoginState extends State<Login> {
         title: const Text('Login Sceen'),
       ),
       body: Center(
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/login_img.png',
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Text("Welcome $name",
-                style: const TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                )),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    TextFormField(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/login_img.png',
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Text("Welcome $name",
+                  style: const TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: 'Enter username',
+                            label: Text('Username'),
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Username cannot be empty!';
+                            }
+                            // returning null as there is no error
+                            return null;
+                          },
+                          onChanged: (value) {
+                            name = value;
+                            setState(() {});
+                          }),
+                      TextFormField(
+                        obscureText: true,
                         decoration: const InputDecoration(
-                          hintText: 'Enter username',
-                          label: Text('Username'),
+                          hintText: 'Enter password',
+                          label: Text('Password'),
                         ),
-                        onChanged: (value) {
-                          name = value;
-                          setState(() {});
-                        }),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        hintText: 'Enter password',
-                        label: Text('Password'),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Password cannot be empty!';
+                          }
+                          return null;
+                        },
                       ),
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    InkWell(
-                      onTap: () async {
-                        // Navigator.pushNamed(context, MyRoutes.homeRoute);
-                        setState(() {
-                          changeButton = true;
-                        });
-                        // await and future used as the change of screen to homepage 
-                        // has to be done after 1s only i.e after the transition of the animatedContainer
-                        await Future.delayed(Duration(seconds: 1));
-                        Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      },
-                      child: AnimatedContainer(
-                        duration: Duration(seconds: 1),
-                        width: changeButton ? 50 : 150,
-                        height: 50,
-                        child: changeButton
-                            ? Icon(
-                                Icons.done, 
-                                color: Colors.white,
-                              )
-                            : Text(
-                                'Login',
-                                style: TextStyle(
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      InkWell(
+                        onTap: () => moveToHome(context),
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          width: changeButton ? 50 : 150,
+                          height: 50,
+                          child: changeButton
+                              ? const Icon(
+                                  Icons.done,
                                   color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
+                                )
+                              : Text(
+                                  'Login',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
                                 ),
-                              ),
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: Colors.deepPurple,
-                          borderRadius:
-                              BorderRadius.circular(changeButton ? 50 : 10),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius:
+                                BorderRadius.circular(changeButton ? 50 : 10),
+                          ),
                         ),
-                      ),
-                    )
-                    // ElevatedButton(
-                    //   onPressed: () {
-                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    //   },
-                    //   style: TextButton.styleFrom(
-                    //     minimumSize: Size(150, 40),
-                    //   ),
-                    //   child: const Text('Login'),
-                    // ),
-                  ],
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:login_app/Pages/home_page.dart';
-
 import 'package:google_fonts/google_fonts.dart';
 import 'package:login_app/utils/routes.dart';
 
